9b2b48cf3ac413437c0dc1c19ffa7b51b3f74b7f
benaiah-ke <benaiahpraise@gmail.com>
2022-10-30 04:50:40 +0300

    completed lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
 
+    def initialize (book_title)
+        @title = book_title
+    end
+
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book_one = Book.new("This book is a new book")
+book_one.author = "Benaiah"
+book_one.page_count = 200
+book_one.genre = "Motivational Books"
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Shoe
+
+    def initialize (shoe_brand)
+        @brand = shoe_brand
+    end
+
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+book_one = Book.new("This book is a new book")
+book_one.author = "Benaiah"
+book_one.page_count = 200
+book_one.genre = "Motivational Books"
